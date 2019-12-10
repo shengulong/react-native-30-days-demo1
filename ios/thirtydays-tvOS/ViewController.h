4f560ff08991bfbe921732fcc8568295cf83826a
shengulong <shengulong@126.com>
2019-12-10 22:14:07 +0800

    add target for ios

---
+++ ios/thirtydays-tvOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  thirtydays-tvOS
+//
+//  Created by 申得普 on 2019/12/10.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
